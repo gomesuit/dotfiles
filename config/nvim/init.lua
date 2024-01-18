974094602be106c5c60db98694d42dca81d70f77
gomesuit <9404411+gomesuit@users.noreply.github.com>
2024-01-18 10:30:21 +0900

    save

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,7 +1,7 @@
 -- vim-plugのインストールチェックとセットアップ
 local plug_path = vim.fn.stdpath('data')..'/site/autoload/plug.vim'
 if vim.fn.empty(vim.fn.glob(plug_path)) > 0 then
-  vim.fn.system({'curl', '-fLo', plug_path, '--create-dirs', 
+  vim.fn.system({'curl', '-fLo', plug_path, '--create-dirs',
                  'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'})
   vim.api.nvim_command('autocmd VimEnter * PlugInstall | source $MYVIMRC')
 end
