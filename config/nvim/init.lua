ee2fcd8362404aaa1cd40c4c966f8a4fd078e5e4
gomesuit <9404411+gomesuit@users.noreply.github.com>
2024-01-18 10:17:55 +0900

    save

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -9,17 +9,35 @@ end
 -- vim-plugプラグインマネージャーを開始
 vim.call('plug#begin', vim.fn.stdpath('data')..'/plugged')
 
--- プラグインのリスト
+-- 追加、削除、変更された行にマーカーを表示する
 vim.call('plug#', 'airblade/vim-gitgutter')
+
+-- git拡張
 vim.call('plug#', 'tpope/vim-fugitive')
+
+-- Vimの画面の一番下にあるステータスラインの表示内容が強化される
 vim.call('plug#', 'bling/vim-airline')
 vim.call('plug#', 'vim-airline/vim-airline-themes')
+
+-- vimでペーストする際に、自動でpaste modeにする
 vim.call('plug#', 'ConradIrwin/vim-bracketed-paste')
+
+-- helper for unix
 vim.call('plug#', 'tpope/vim-eunuch')
+
+-- タブとスペースの自動検知
 vim.call('plug#', 'tpope/vim-sleuth')
+
+-- A collection of language packs for Vim.
 vim.call('plug#', 'sheerun/vim-polyglot')
+
+-- Lint Engine
 vim.call('plug#', 'dense-analysis/ale')
+
+-- commentout shortcut, Use gc
 vim.call('plug#', 'tpope/vim-commentary')
+
+-- ddc.vim (auto-completion)
 vim.call('plug#', 'Shougo/ddc.vim')
 vim.call('plug#', 'Shougo/pum.vim')
 vim.call('plug#', 'vim-denops/denops.vim')
@@ -27,13 +45,18 @@ vim.call('plug#', 'Shougo/ddc-ui-native')
 vim.call('plug#', 'Shougo/ddc-source-around')
 vim.call('plug#', 'Shougo/ddc-matcher_head')
 vim.call('plug#', 'Shougo/ddc-sorter_rank')
+
+-- ディレクトリをツリー表示
 vim.call('plug#', 'preservim/nerdtree')
 vim.call('plug#', 'jistr/vim-nerdtree-tabs')
+
+-- terminal
 vim.call('plug#', 'akinsho/toggleterm.nvim', {['tag'] = '*'})
 
 vim.call('plug#end')
 
 -- toggletermの設定
+-- https://github.com/akinsho/toggleterm.nvim
 require("toggleterm").setup{
   open_mapping = [[<c-\>]],
   direction = "vertical",
@@ -71,30 +94,52 @@ end
 
 -- 基本的なオプション設定
 vim.o.swapfile = false
+-- カーソルが何行目の何列目に置かれているかを表示する
 vim.o.ruler = true
+-- コマンドラインに使われる画面上の行数
 vim.o.cmdheight = 2
+-- エディタウィンドウの末尾から2行目にステータスラインを常時表示させる
 vim.o.laststatus = 2
+-- ステータス行に表示させる情報の指定
 vim.o.statusline = "%<%f %m%r%h%w%{'['.(&fenc!=''?&fenc:&enc).']['.&ff.']'}%=%l,%c%V%8P"
+-- ウインドウのタイトルバーにファイルのパス情報等を表示する
 vim.o.title = true
+-- コマンドラインモードで<Tab>キーによるファイル名補完を有効にする
 vim.o.wildmenu = true
+-- 入力中のコマンドを表示する
 vim.o.showcmd = true
 vim.o.browsedir = 'buffer'
+-- 小文字のみで検索したときに大文字小文字を無視する
 vim.o.smartcase = true
+-- 検索結果をハイライト表示する
 vim.o.hlsearch = true
+-- 暗い背景色に合わせた配色にする
 vim.o.background = 'dark'
+-- タブ入力を複数の空白入力に置き換える
 vim.o.expandtab = true
+-- 検索ワードの最初の文字を入力した時点で検索を開始する
 vim.o.incsearch = true
 vim.o.hidden = true
 vim.o.list = true
+-- タブと行の続きを可視化する
 vim.o.listchars = 'tab:>\\ ,extends:<'
+-- 行番号を表示する
 vim.o.number = true
+-- 対応する括弧やブレースを表示する
 vim.o.showmatch = true
+-- 改行時に前の行のインデントを継続する
 vim.o.autoindent = true
+-- 改行時に入力された行の末尾に合わせて次の行のインデントを増減する
 vim.o.smartindent = true
+-- タブ文字の表示幅
 vim.o.tabstop = 2
+-- Vimが挿入するインデントの幅
 vim.o.shiftwidth = 2
+-- 行頭の余白内で Tab を打ち込むと、'shiftwidth' の数だけインデントする
 vim.o.smarttab = true
+-- カーソルを行頭、行末で止まらないようにする
 vim.o.whichwrap = 'b,s,h,l,<,>,[,]'
+-- 行番号の色
 vim.api.nvim_command('highlight LineNr ctermfg=darkyellow')
 vim.o.backspace = 'indent,eol,start'
 
