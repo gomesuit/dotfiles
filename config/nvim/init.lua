0b92ff789113101835c06ed568381fb72e9904f9
gomesuit <9404411+gomesuit@users.noreply.github.com>
2024-01-27 13:42:43 +0900

    save

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -62,6 +62,8 @@ vim.call('plug#', 'akinsho/toggleterm.nvim', {['tag'] = '*'})
 vim.call('plug#', 'nvim-lua/plenary.nvim')
 vim.call('plug#', 'nvim-telescope/telescope.nvim', {['tag'] = '0.1.5'})
 vim.call('plug#', 'nvim-treesitter/nvim-treesitter')
+vim.call('plug#', 'nvim-telescope/telescope-fzf-native.nvim', {['do'] = 'make'})
+
 
 -- vim-terraform
 vim.call('plug#', 'hashivim/vim-terraform')
@@ -166,7 +168,20 @@ require('telescope').setup{
       }
     }
   },
+  extensions = {
+    fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = true,  -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
+    }
+  },
 }
+
+-- To get fzf loaded and working with telescope, you need to call
+-- load_extension, somewhere after setup function:
+require('telescope').load_extension('fzf')
 
 -- vim-terraform
 vim.g.terraform_align = 1
