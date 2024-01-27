8f71b29c2238cdef0bfce337d4a9c7f74374afac
gomesuit <9404411+gomesuit@users.noreply.github.com>
2024-01-27 15:03:12 +0900

    save

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,76 +1,7 @@
 -- set Leader key
 vim.g.mapleader = ' '
 
--- vim-plugのインストールチェックとセットアップ
-local plug_path = vim.fn.stdpath('data')..'/site/autoload/plug.vim'
-if vim.fn.empty(vim.fn.glob(plug_path)) > 0 then
-  vim.fn.system({'curl', '-fLo', plug_path, '--create-dirs',
-                 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'})
-  vim.api.nvim_command('autocmd VimEnter * PlugInstall | source $MYVIMRC')
-end
-
--- vim-plugプラグインマネージャーを開始
-vim.call('plug#begin', vim.fn.stdpath('data')..'/plugged')
-
--- 追加、削除、変更された行にマーカーを表示する
-vim.call('plug#', 'airblade/vim-gitgutter')
-
--- git拡張
-vim.call('plug#', 'tpope/vim-fugitive')
-
--- Vimの画面の一番下にあるステータスラインの表示内容が強化される
-vim.call('plug#', 'bling/vim-airline')
-vim.call('plug#', 'vim-airline/vim-airline-themes')
-
--- helper for unix
-vim.call('plug#', 'tpope/vim-eunuch')
-
--- タブとスペースの自動検知
-vim.call('plug#', 'tpope/vim-sleuth')
-
--- A collection of language packs for Vim.
-vim.call('plug#', 'sheerun/vim-polyglot')
-
--- Lint Engine
-vim.call('plug#', 'dense-analysis/ale')
-
--- commentout shortcut, Use gc
-vim.call('plug#', 'tpope/vim-commentary')
-
--- ddc.vim (auto-completion)
--- require deno (https://deno.com/)
-vim.call('plug#', 'Shougo/ddc.vim')
-vim.call('plug#', 'Shougo/pum.vim')
-vim.call('plug#', 'vim-denops/denops.vim')
-vim.call('plug#', 'Shougo/ddc-ui-native')
-vim.call('plug#', 'Shougo/ddc-source-around')
-vim.call('plug#', 'Shougo/ddc-matcher_head')
-vim.call('plug#', 'Shougo/ddc-sorter_rank')
-
--- ディレクトリをツリー表示
-vim.call('plug#', 'preservim/nerdtree')
-vim.call('plug#', 'jistr/vim-nerdtree-tabs')
-
--- require https://github.com/ryanoasis/nerd-fonts/
-vim.call('plug#', 'nvim-tree/nvim-web-devicons')
-vim.call('plug#', 'nvim-tree/nvim-tree.lua')
-
--- terminal
-vim.call('plug#', 'akinsho/toggleterm.nvim', {['tag'] = '*'})
-
--- telescope
-vim.call('plug#', 'nvim-lua/plenary.nvim')
-vim.call('plug#', 'nvim-telescope/telescope.nvim', {['tag'] = '0.1.5'})
-vim.call('plug#', 'nvim-treesitter/nvim-treesitter')
-vim.call('plug#', 'nvim-telescope/telescope-fzf-native.nvim', {['do'] = 'make'})
-
-
--- vim-terraform
-vim.call('plug#', 'hashivim/vim-terraform')
-
--- vim.call('plug#', 'neoclide/coc.nvim', {['branch'] = 'release'})
-
-vim.call('plug#end')
+require('plugins')
 
 -- toggletermの設定
 -- https://github.com/akinsho/toggleterm.nvim
@@ -188,8 +119,8 @@ vim.g.terraform_align = 1
 vim.g.terraform_fmt_on_save = 1
 
 -- 基本的なオプション設定
-require("options")
+require('options')
 
 -- キーマッピング
-require("keymaps")
+require('keymaps')
 
