dfeee9b69f4d61f6e32af31f5d3db309717c19ec
gomesuit <9404411+gomesuit@users.noreply.github.com>
2024-02-04 19:59:54 +0900

    save

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,10 +1,10 @@
 -- set Leader key
 vim.g.mapleader = ' '
 
-require('_plugins/init')
-require('options')
-require('keymaps')
+-- require('_plugins/init')
+-- require('options')
+-- require('keymaps')
 
--- require("config.lazy")
--- require('config.options')
--- require('config.keymaps')
+require("config.lazy")
+require('config.options')
+require('config.keymaps')
