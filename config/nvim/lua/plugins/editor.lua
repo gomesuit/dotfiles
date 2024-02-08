d0c4f520d57da4b83f99d4fbfb26ab745ee105d7
gomesuit <9404411+gomesuit@users.noreply.github.com>
2024-02-09 01:39:38 +0900

    save

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -61,5 +61,42 @@ return {
         },
       })
     end,
+  },
+
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "lukas-reineke/lsp-format.nvim",
+    },
+
+    config = function()
+      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+      local null_ls = require("null-ls")
+
+      null_ls.setup({
+        sources = {
+          -- formatting
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.terraform_fmt,
+
+          -- diagnostics
+          null_ls.builtins.diagnostics.eslint,
+
+          -- completion
+          null_ls.builtins.completion.spell,
+        },
+        on_attach = require("lsp-format").on_attach
+      })
+    end,
+  },
+
+  {
+    "lukas-reineke/lsp-format.nvim",
+
+    config = function()
+      require("lsp-format").setup({})
+    end,
   }
 }
