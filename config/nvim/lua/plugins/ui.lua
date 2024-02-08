8adc670cf56eb720836699f5747c7f0c7c1305b5
gomesuit <9404411+gomesuit@users.noreply.github.com>
2024-02-09 00:12:30 +0900

    save

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -257,12 +257,12 @@ return {
   {
     "kevinhwang91/nvim-hlslens",
 
-    config = function()
-      -- require('hlslens').setup() is not required
-      require("scrollbar.handlers.search").setup({
+  config = function()
+    -- require('hlslens').setup() is not required
+    require("scrollbar.handlers.search").setup({
         -- hlslens config overrides
-      })
-    end,
+    })
+  end,
   },
 
   -- better diagnostics list and others
