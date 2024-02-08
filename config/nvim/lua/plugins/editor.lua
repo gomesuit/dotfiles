ba9b7a33409c11161dafde2ca9e146e6978db910
gomesuit <9404411+gomesuit@users.noreply.github.com>
2024-02-09 00:58:45 +0900

    save

--- config/nvim/lua/plugins/editor.lua
+++ config/nvim/lua/plugins/editor.lua
@@ -11,4 +11,39 @@ return {
     },
     lazy = false,
   },
+
+  {
+    "andymass/vim-matchup",
+    setup = function()
+      vim.g.matchup_matchparen_offscreen = { method = "popup" }
+
+      require'nvim-treesitter.configs'.setup {
+        matchup = {
+          enable = true,              -- mandatory, false will disable the whole extension
+          -- disable = { "c", "ruby" },  -- optional, list of language that will be disabled
+          -- [options]
+        },
+      }
+    end,
+  },
+
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    opts = {} -- this is equalent to setup({}) function
+  },
+
+  {
+    "windwp/nvim-ts-autotag",
+
+    config = function()
+      require('nvim-ts-autotag').setup()
+
+      require'nvim-treesitter.configs'.setup {
+        autotag = {
+          enable = true,
+        }
+      }
+    end,
+  }
 }
