3b9d18fb6e2d1d3e4c6ddf16ccd21d3d8ede5e40
gomesuit <9404411+gomesuit@users.noreply.github.com>
2024-08-20 17:48:21 +0900

    save

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -1,6 +1,7 @@
 return {
   {
     "folke/noice.nvim",
+    commit = "d9328ef903168b6f52385a751eb384ae7e906c6f",
     event = "VeryLazy",
     dependencies = {
       -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
